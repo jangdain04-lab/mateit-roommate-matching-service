9e46814aa1447e2364c8ef213e3cdbc00b6717b5
Jang Dain <jangdain04@gmail.com>
2026-09-18 16:04:43 +0900

    feat: complete Mate It portfolio source

---
+++ utils/label_dict.py
@@ -0,0 +1,72 @@
+# utils/label_dict.py
+
+STYLE_LABELS = {
+    'daily_rhythm': {
+        'morning': '#아침형',
+        'night': '#저녁형',
+    },
+    'wake_habit': {
+        'hard': '#알람필수',
+        'easy': '#바로기상',
+    },
+    'weekend_life': {
+        'home': '#본가러',
+        'dorm': '#기숙사러',
+    },
+    'sleep_habit': {
+        'talk': '#잠꼬대있음',
+        'quiet': '#조용한수면',
+    },
+    'personality_type': {
+        'calm': '#조용한성격',
+        'talkative': '#말많은성격',
+    },
+    'opinion_style': {
+        'direct': '#직설적',
+        'indirect': '#돌려말함',
+    },
+    'privacy_need': {
+        'important': '#개인공간중시',
+        'not_mind': '#공간공유관대',
+    },
+    'scent_sensitivity': {
+        'sensitive': '#냄새예민',
+        'not_sensitive': '#냄새관대',
+    },
+    'earphone_use': {
+        'often': '#이어폰자주사용',
+        'rarely': '#이어폰거의안씀',
+    },
+    'preferred_temp': {
+        'warm': '#따뜻한방',
+        'cool': '#시원한방',
+    },
+    'cleaning_style': {
+        'clean': '#청소자주함',
+        'not_clean': '#청소신경X',
+    },
+    'study_place': {
+        'dorm': '#기숙사공부',
+        'library': '#도서관공부',
+    },
+    'shower_time': {
+        'morning': '#아침샤워',
+        'night': '#저녁샤워',
+    },
+    'shared_item_opinion': {
+        'okay': '#함께구매',
+        'no': '#각자구매',
+    },
+    'outing_frequency': {
+        'frequent': '#외출자주',
+        'rare': '#외출가끔',
+    },
+    'smoking': {
+        'yes': '#흡연자',
+        'no': '#비흡연자',
+    },
+    'military': {
+        'yes': '#군필',
+        'no': '#미필',
+    },
+}
